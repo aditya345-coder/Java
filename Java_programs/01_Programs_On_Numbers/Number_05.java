7702e37c32c5181a3f3ca923638995d94fdfe5b5
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-28 20:05:15 +0530

    Update Number_05.java
    Added sc.close and comments.

--- Java_programs/01_Programs_On_Numbers/Number_05.java
+++ Java_programs/01_Programs_On_Numbers/Number_05.java
@@ -1,30 +1,29 @@
 // Write a program to print prime numbers from 1 to n
+/* Prime numbers : A prime number is a natural number greater than 1 and 
+                   a number that is divisible only by itself and 1 (e.g. 2, 3, 5, 7, 11)
+ */
 
-public class Number_05 
-{
-    public static void main(String[] args)
-    {
+public class Number_05 {
+    public static void main(String[] args) {
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        // Taking user input
+        // taking user input
         System.out.print("Enter the value of n: ");
         int num = sc.nextInt();
-        // Displaying prime numbers between 1 to n
+
         System.out.printf("The prime numbers from 1 to %d are: \n", num);
-        for (int i = 2; i <= num; i++) 
-        {
+        for (int i = 2; i <= num; i++) {
             int count = 0;
-            for (int j = 2; j <= i / 2; j++)
-            {
-                if (i % j == 0) 
-                {
+
+            for (int j = 2; j <= i / 2; j++) {
+                if (i % j == 0) {
                     count++;
                     break;
                 }
             }
-            if (count == 0)
-            {
+            if (count == 0) {
                 System.out.print(i + " ");
             }
         }
+        sc.close();
     }
 }
