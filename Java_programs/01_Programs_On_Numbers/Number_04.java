c64ef6083a761ed6eccaad1b448a985dcdbd3e30
Aditya Gaharwar <92220550+aditya345-coder@users.noreply.github.com>
2022-10-28 20:20:12 +0530

    Update Number_04.java
    Added comments.

--- Java_programs/01_Programs_On_Numbers/Number_04.java
+++ Java_programs/01_Programs_On_Numbers/Number_04.java
@@ -1,4 +1,9 @@
-// Write a program to check the given number is even or odd ?
+// Write a program to check the given number is even or odd 
+/* Odd numbers: Odd numbers are those numbers that cannot be divided into two equal parts. 
+                 Example: 3, 5, 7, 9, 11, 13, 15,… 
+   Even numbers: Even numbers are those numbers that can be divided into two equal parts.
+                 Example: 2, 4, 6, 8, 10, 12, 14,…
+ */
 
 import java.util.Scanner;
 
